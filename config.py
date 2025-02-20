98132115fdb853fecf87fe2e2be1e780515103dd
Mike <mikhail.vershkov@gmail.com>
2025-02-20 21:58:46 +0300

    `Refactor log configuration: introduce LOG_DIR and update LOG_FILENAME`

--- config.py
+++ config.py
@@ -7,12 +7,10 @@ from dotenv import load_dotenv
 # Загрузка переменных окружения
 load_dotenv()
 
-# Флаг тестового режима
-TEST_MODE = False  # Установите False для продакшена
+LOG_DIR = "./logs"  # Путь к директории логов
+LOG_FILENAME = os.path.join(LOG_DIR, "bot.log")
 
-# Выбор имени файла лога в зависимости от режима
-LOG_FILENAME = 'test.log' if TEST_MODE else 'pulse.log'
-
+os.makedirs(LOG_DIR, exist_ok=True)
 # Настройка логирования
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -33,15 +31,9 @@ console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
-# Переменные API_TOKEN и COMPANY_CHANNEL_ID в зависимости от режима
-if TEST_MODE:
-    API_TOKEN = os.getenv("TEST_TELEGRAM_API_TOKEN")
-    COMPANY_CHANNEL_ID = int(os.getenv("TEST_CHANNEL_ID"))
-else:
-    API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
-    COMPANY_CHANNEL_ID = int(os.getenv("COMPANY_CHANNEL_ID"))
 
-# Остальные переменные (общие для обоих режимов)
+API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+COMPANY_CHANNEL_ID = int(os.getenv("COMPANY_CHANNEL_ID"))
 WORK_MAIL = os.getenv("WORK_MAIL")
 UNI_API_KEY = os.getenv("UNI_API_KEY")
 UNI_EMAIL = os.getenv("UNI_EMAIL")
